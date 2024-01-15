0418e9e4897db73a1b815bdce3020775e482b6f7
NicoleBowen233 <156719993+NicoleBowen233@users.noreply.github.com>
2024-01-15 15:32:52 -0700

    Update streamlit_app.py
    Connect to data library and read smoothie items into the smoothie section.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
-import streamlit
-streamlit.title('My Moms New Healthy Diner');
 
+import pandas
+import streamlit;
+
+streamlit.title('My Moms New Healthy Diner');
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -8,7 +10,10 @@ streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast');
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇');
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+;
 
   
 
